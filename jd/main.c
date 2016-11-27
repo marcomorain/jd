2415928127588dbaf3bbfd234a5c19f90a0a7af0
Marc O'Morain <github.com@marcomorain.com>
2016-11-27 16:13:44 +0000

    Initial Commit

---
+++ jd/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  jd
+//
+//  Created by Marc O'Morain on 27/11/2016.
+//  Copyright © 2016 Marc O'Morain. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
